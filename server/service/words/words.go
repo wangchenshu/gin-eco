18b473969c40e9ae6678d7e0bf268ae44548872b
Chen Shu <csw.dawn@gmail.com>
2020-01-05 15:14:30 +0800

    add quick reply

--- server/service/words/words.go
+++ server/service/words/words.go
@@ -14,8 +14,29 @@ import (
 	"gin-eco/server/service/mylinebot"
 )
 
+const (
+	// WordStr -
+	WordStr = "好語"
+	// WisdomAdageStr -
+	WisdomAdageStr = "自在語"
+	// PhorismStr -
+	PhorismStr = "靜思語"
+	// InspirationalStr -
+	InspirationalStr = "勵志語"
+)
+
+// TemplateTitle - template title
+const TemplateTitle = "禪念 Bot Go 2.0"
+
+// DefaultImg - default img
+const DefaultImg = "https://firebasestorage.googleapis.com/v0/b/walter-bot-a2142.appspot.com/o/line-bot%2Fimage%2Fother%2Fgirl_img%2F27367-5nYPUB.jpg?alt=media&token=9ec89929-5b2d-478c-b8da-c37f61f338a0"
+
+// WordsLimit -
+const WordsLimit = 200
+
 var myBot = mylinebot.Init()
 
+// GetWords - get words
 func GetWords() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		events, err := myBot.ParseRequest(context.Request)
@@ -34,36 +55,34 @@ func GetWords() gin.HandlerFunc {
 			case linebot.EventTypeMessage:
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					repMsg := "請輸入好語，自在語 或是 靜思語"
-
-					if message.Text == "好語" {
+					defaultMsg := fmt.Sprintf("請輸入%s，%s 或是 %s", WordStr, WisdomAdageStr, PhorismStr)
+					repMsg := defaultMsg
+					if message.Text == WordStr {
 						repMsg = wordsRandWords()
-					} else if message.Text == "自在語" {
+					} else if message.Text == WisdomAdageStr {
 						repMsg = wisdomAdagesRandWords()
-					} else if message.Text == "靜思語" {
+					} else if message.Text == PhorismStr {
 						repMsg = phorismsRandWords()
-					} else if message.Text == "勵志語" {
+					} else if message.Text == InspirationalStr {
 						repMsg = inspirationalsRandWords()
 					}
-					imageURL := "https://firebasestorage.googleapis.com/v0/b/walter-bot-a2142.appspot.com/o/line-bot%2Fimage%2Fother%2Fgirl_img%2F27367-5nYPUB.jpg?alt=media&token=9ec89929-5b2d-478c-b8da-c37f61f338a0"
+
+					imageURL := DefaultImg
 					template := linebot.NewButtonsTemplate(
-						imageURL, "禪念 Bot Go 1.0", repMsg,
-						linebot.NewMessageAction("好語", "好語"),
-						linebot.NewMessageAction("自在語", "自在語"),
-						linebot.NewMessageAction("靜思語", "靜思語"),
-						linebot.NewMessageAction("勵志語", "勵志語"),
+						imageURL, TemplateTitle, defaultMsg,
+						linebot.NewMessageAction(WordStr, WordStr),
+						linebot.NewMessageAction(WisdomAdageStr, WisdomAdageStr),
+						linebot.NewMessageAction(PhorismStr, PhorismStr),
+						linebot.NewMessageAction(InspirationalStr, InspirationalStr),
 					)
 					if _, err := myBot.ReplyMessage(
 						event.ReplyToken,
 						linebot.NewTemplateMessage(repMsg, template),
 						linebot.NewTextMessage(repMsg),
+						myQuickReply(),
 					).Do(); err != nil {
 						log.Print(err)
 					}
-
-					// if _, err = myBot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(repMsg)).Do(); err != nil {
-					// 	log.Print(err)
-					// }
 				case *linebot.ImageMessage:
 					log.Print(message)
 				case *linebot.VideoMessage:
@@ -91,32 +110,55 @@ func GetWords() gin.HandlerFunc {
 
 func wordsRandWords() string {
 	words := model.EcoWords{}
-	db.Db.Raw("SELECT * FROM eco_words where LENGTH(words) < ? ORDER BY RAND() LIMIT 1", 60).Scan(&words)
+	db.Db.Raw("SELECT * FROM eco_words where LENGTH(words) < ? ORDER BY RAND() LIMIT 1", WordsLimit).Scan(&words)
 
 	return words.Words
 }
 
 func wisdomAdagesRandWords() string {
 	words := model.EcoWisdomAdages{}
-	db.Db.Raw("SELECT * FROM eco_wisdom_adages where LENGTH(words) < ? ORDER BY RAND() LIMIT 1", 60).Scan(&words)
+	db.Db.Raw("SELECT * FROM eco_wisdom_adages where LENGTH(words) < ? ORDER BY RAND() LIMIT 1", WordsLimit).Scan(&words)
 
 	return words.Words
 }
 
 func inspirationalsRandWords() string {
 	words := model.EcoInspirationals{}
-	db.Db.Raw("SELECT * FROM eco_inspirationals where LENGTH(words) < ? ORDER BY RAND() LIMIT 1", 60).Scan(&words)
+	db.Db.Raw("SELECT * FROM eco_inspirationals where LENGTH(words) < ? ORDER BY RAND() LIMIT 1", WordsLimit).Scan(&words)
 
 	return words.Words
 }
 
 func phorismsRandWords() string {
 	words := model.EcoPhorisms{}
-	db.Db.Raw("SELECT * FROM eco_phorisms where LENGTH(words) < ? ORDER BY RAND() LIMIT 1", 60).Scan(&words)
+	db.Db.Raw("SELECT * FROM eco_phorisms where LENGTH(words) < ? ORDER BY RAND() LIMIT 1", WordsLimit).Scan(&words)
 
 	return words.Words
 }
 
+func myQuickReply() linebot.SendingMessage {
+	content := fmt.Sprintf("快速選單")
+	imageURLs := []string{
+		"https://firebasestorage.googleapis.com/v0/b/walter-bot-a2142.appspot.com/o/line-bot%2Fimage%2Fother%2Fzen%2F3.png?alt=media&token=44770a07-a661-40dc-960e-45da1699e4f2",
+		"https://firebasestorage.googleapis.com/v0/b/walter-bot-a2142.appspot.com/o/line-bot%2Fimage%2Fother%2Fzen%2F3.png?alt=media&token=44770a07-a661-40dc-960e-45da1699e4f2",
+		"https://firebasestorage.googleapis.com/v0/b/walter-bot-a2142.appspot.com/o/line-bot%2Fimage%2Fother%2Fzen%2F3.png?alt=media&token=44770a07-a661-40dc-960e-45da1699e4f2",
+		"https://firebasestorage.googleapis.com/v0/b/walter-bot-a2142.appspot.com/o/line-bot%2Fimage%2Fother%2Fzen%2F3.png?alt=media&token=44770a07-a661-40dc-960e-45da1699e4f2",
+	}
+	labels := []string{WordStr, WisdomAdageStr, PhorismStr, InspirationalStr}
+	quickReplyButtons := []*linebot.QuickReplyButton{}
+
+	for k, v := range labels {
+		quickReplyButtons = append(quickReplyButtons, linebot.NewQuickReplyButton(
+			imageURLs[k], linebot.NewMessageAction(v, v),
+		))
+	}
+	quickReply := linebot.NewTextMessage(content).
+		WithQuickReplies(linebot.NewQuickReplyItems(quickReplyButtons...))
+
+	return quickReply
+}
+
+// GetEcoWords -
 func GetEcoWords() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		words := []model.EcoWords{}
@@ -126,12 +168,14 @@ func GetEcoWords() gin.HandlerFunc {
 	}
 }
 
+// GetEcoWordsRand -
 func GetEcoWordsRand() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, wordsRandWords())
 	}
 }
 
+// GetEcoWisdomAdages -
 func GetEcoWisdomAdages() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		words := []model.EcoWisdomAdages{}
@@ -141,12 +185,14 @@ func GetEcoWisdomAdages() gin.HandlerFunc {
 	}
 }
 
+// GetEcoWisdomAdagesRand -
 func GetEcoWisdomAdagesRand() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, wisdomAdagesRandWords())
 	}
 }
 
+// GetEcoInspirationals -
 func GetEcoInspirationals() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		words := []model.EcoInspirationals{}
@@ -156,12 +202,14 @@ func GetEcoInspirationals() gin.HandlerFunc {
 	}
 }
 
+// GetEcoInspirationalsRand -
 func GetEcoInspirationalsRand() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, inspirationalsRandWords())
 	}
 }
 
+// GetEcoPhorisms -
 func GetEcoPhorisms() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		words := []model.EcoPhorisms{}
@@ -171,6 +219,7 @@ func GetEcoPhorisms() gin.HandlerFunc {
 	}
 }
 
+// GetEcoPhorismsRand -
 func GetEcoPhorismsRand() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, phorismsRandWords())
